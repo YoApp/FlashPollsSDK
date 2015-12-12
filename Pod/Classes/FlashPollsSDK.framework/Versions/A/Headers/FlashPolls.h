0f74849af0b23d424c460f7973d33c62be0fd531
Or Arbel <orarbel@gmail.com>
2015-12-12 13:56:28 -0800

    .

---
+++ Pod/Classes/FlashPollsSDK.framework/Versions/A/Headers/FlashPolls.h
@@ -0,0 +1,15 @@
+//
+//  Flash_Polls_SDK.h
+//  Flash Polls SDK
+//
+//  Created by mac on 12/11/15.
+//  Copyright © 2015 Life Before Us, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FlashPolls : NSObject
+
++ (void)init;
+
+@end
